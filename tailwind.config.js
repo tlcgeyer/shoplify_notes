3cf6aecb154d670bc8a1cf895a91307dcd221b01
Tamlin Geyer <tamlincupido2402@gmail.com>
2024-06-19 16:05:26 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/* make use of 'npx tailwindcss init' to config the file */
